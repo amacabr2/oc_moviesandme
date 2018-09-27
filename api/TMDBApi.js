64a884a61be03327e60255e14195159e415db583
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-27 18:40:27 +0200

    Récupération des films depuis l'API + affichage

---
+++ api/TMDBApi.js
@@ -0,0 +1,11 @@
+const API_TOKEN = "cbd10c0e49977aa4063ebd7fc84aea37"
+const LANGUAGE = "fr-FR"
+
+export function getFilmsFromApiWithSearchedText(text) {
+    //https://api.themoviedb.org/3/movie/550?api_key
+    const url = 'https://api.themoviedb.org/3/search/movie?api_key=' + API_TOKEN + '&language=' + LANGUAGE + '&query=' + text
+
+    return fetch(url)
+        .then((response) => response.json())
+        .catch((error) => console.error(error))
+}
--- components/Search.js
+++ components/Search.js
@@ -1,18 +1,47 @@
 import  React from 'react'
-import {Button, TextInput, View, StyleSheet, FlatList, Text} from "react-native";
-import films from '../helpers/FilmsData'
+import {Button, TextInput, View, StyleSheet, FlatList} from "react-native";
 import FilmItem from "./FilmItem";
+import { getFilmsFromApiWithSearchedText } from '../api/TMDBApi'
 
 export default class Search extends React.Component {
+
+    constructor(props) {
+        super(props)
+        this.searchedText = ""
+        this.state = {
+            films: []
+        }
+    }
+
+    _loadFilms() {
+        console.log(this.searchedText.length)
+        if (this.searchedText.length > 0) {
+            getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+                this.setState({films: data.results})
+            })
+        }
+    }
+
+    _searchTextInputChanged(text) {
+        this.searchedText = text
+    }
 
     render() {
         return (
             <View style={styles.main_container}>
-                <TextInput style={styles.textinput} placeholder='Titre du film'/>
-                <Button style={styles.button} title='Rechercher' onPress={() => {}}/>
+                <TextInput
+                    style={styles.textinput}
+                    placeholder='Titre du film'
+                    onChangeText={(text) => this._searchTextInputChanged(text)}
+                />
+                <Button
+                    style={styles.button}
+                    title='Rechercher'
+                    onPress={() => this._loadFilms()}
+                />
 
                 <FlatList
-                    data={films}
+                    data={this.state.films}
                     keyExtractor={(item) => item.id.toString()}
                     renderItem={({item}) => <FilmItem film={item}/>}
                 />

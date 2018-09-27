188bf0e34707d35841fbddb6d056f478e193e4e8
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-27 17:15:26 +0200

    Style pour le formulaire

--- components/Search.js
+++ components/Search.js
@@ -1,14 +1,30 @@
 import  React from 'react'
-import {Button, TextInput, View} from "react-native";
+import {Button, TextInput, View, StyleSheet} from "react-native";
 
 export default class Search extends React.Component {
 
     render() {
         return(
-            <View>
-                <TextInput placeholder='Titre du film' />
-                <Button title='Rechercher' onPress={_ => {}}/>
+            <View style={styles.view}>
+                <TextInput style={styles.textinput} placeholder='Titre du film' />
+                <Button style={styles.button} title='Rechercher' onPress={_ => {}}/>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    view: {
+        marginTop: 20
+    },
+    textinput: {
+        marginLeft: 5,
+        marginRight: 5,
+        height: 50,
+        paddingLeft: 5
+    },
+    button: {
+        marginLeft: 5,
+        marginRight: 5
+    }
+})

ab4f1b980ed1fb866c1d00e61946c0ef4370aeeb
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-27 17:40:06 +0200

    Création d'item pour affcher la liste de films

--- components/Search.js
+++ components/Search.js
@@ -1,30 +1,38 @@
 import  React from 'react'
-import {Button, TextInput, View, StyleSheet} from "react-native";
+import {Button, TextInput, View, StyleSheet, FlatList, Text} from "react-native";
+import films from '../helpers/FilmsData'
+import FilmItem from "./FilmItem";
 
 export default class Search extends React.Component {
 
     render() {
-        return(
-            <View style={styles.view}>
-                <TextInput style={styles.textinput} placeholder='Titre du film' />
-                <Button style={styles.button} title='Rechercher' onPress={_ => {}}/>
+        return (
+            <View style={styles.main_container}>
+                <TextInput style={styles.textinput} placeholder='Titre du film'/>
+                <Button style={styles.button} title='Rechercher' onPress={() => {}}/>
+
+                <FlatList
+                    data={films}
+                    keyExtractor={(item) => item.id.toString()}
+                    renderItem={({item}) => <FilmItem film={item}/>}
+                />
             </View>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    view: {
-        marginTop: 20
-    },
-    textinput: {
+    main_container: {
+        flex: 1,
+        marginTop: 20,
         marginLeft: 5,
         marginRight: 5,
+    },
+    textinput: {
         height: 50,
         paddingLeft: 5
     },
     button: {
-        marginLeft: 5,
-        marginRight: 5
+        height: 50,
     }
 })

8637bfa2c6f8d16f2ae91a301c23cd14e7e9999e
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-28 11:03:03 +0200

    Création du reducer pour le système de films favoris

---
+++ store/reducers/FavoriteReducer.js
@@ -0,0 +1,26 @@
+const initialeState = { favoritesFilm: []}
+
+function toggleFavorite(state = initialeState, action) {
+    let nextState
+
+    switch (action.type) {
+        case 'TOGGLE_FAVORITE':
+            const favoriteFilmIndex = state.favoritesFilm.findIndex(item => item.id === action.value.id)
+
+            if (favoriteFilmIndex !== -1) {
+                nextState = {
+                    ...state,
+                    favoritesFilm: state.favoritesFilm.filter((item, index) => index !== favoriteFilmIndex)
+                }
+            } else {
+                nextState = {
+                    ...state,
+                    favoritesFilm: [...state.favoritesFilm, action.value]
+                }
+            }
+
+            return nextState || state
+        default:
+            return state
+    }
+}

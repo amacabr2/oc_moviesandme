e5dc9607939c54cf350201031b2a555ae5c60bc3
amacabr2 <anthony.macabrey@edu.univ-fcomte.fr>
2018-09-28 15:46:24 +0200

    Persistance des données

--- App.js
+++ App.js
@@ -2,13 +2,19 @@ import React from 'react';
 import Navigation from "./navigations/Navigation";
 import {Provider} from "react-redux";
 import Store from './store/ConfigureStore'
+import { persistStore } from 'redux-persist'
+import { PersistGate } from 'redux-persist/es/integration/react'
 
 export default class App extends React.Component {
     render() {
+        let persistor = persistStore(Store)
+
         return (
             <Provider store={Store}>
-                <Navigation/>
+                <PersistGate persistor={persistor}>
+                    <Navigation />
+                </PersistGate>
             </Provider>
-        );
+        )
     }
 }
--- store/ConfigureStore.js
+++ store/ConfigureStore.js
@@ -1,5 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import toggleFavorite from './reducers/FavoriteReducer'
 import setAvatar from "./reducers/AvatarReducer";
+import { persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
-export default createStore(combineReducers({toggleFavorite, setAvatar}))
+const rootPersistConfig = {
+    key: 'root',
+    storage: storage
+}
+
+export default createStore(persistCombineReducers(rootPersistConfig, {toggleFavorite, setAvatar}))
